2f94d34ff0691f8d31d54f5ae2d6d725b26e29ca
Athena Hernandez <athena.hernandez.23@bishops.com>
2023-03-10 18:39:55 -0500

    Added missing zero padding

--- cnn.py
+++ cnn.py
@@ -10,15 +10,14 @@ from tensorflow.image import rgb_to_grayscale
 import numpy as np
 import pickle
 
-
-
 # Change numbers in dense layers, batch size, learning rate, data augmentation (adding more images)
 train = utils.image_dataset_from_directory(
     'images',
     labels = 'inferred',
     label_mode = 'categorical',
     class_names = None,
-    color_mode = 'grayscale',
+    # Images were gray legos, but still RGB so I grayscaled them
+    color_mode = 'grayscale',       
     batch_size = 32,
     image_size = (200, 200), 
     shuffle = True,
@@ -43,45 +42,67 @@ test = utils.image_dataset_from_directory(
 
 # # Data Augmentation Section
 # # You can have multiple layers, but first one always needs the input_shape
-# rotation = models.Sequential([
-#     layers.RandomRotation(0.25, input_shape = (250, 250, 3))
-# ])
+rotation = models.Sequential([
+    layers.RandomRotation(0.25, input_shape = (200, 200, 1))
+])
 # # train.map() applies the transformation in parentheses to each pair x,y 
 # # in the dataset.  We only need to transform the x-values, we just pass
 # # the y-values along passively.  Notice that the output of the lambda
 # # function is a 2-tuple, which is the transformed image followed
-# rotated = train.map(lambda x, y: (rotation(x), y))
+rotated = train.map(lambda x, y: (rotation(x), y))
 # # Make sure you create *all* the transformed copies before assembling
 # # them into a new training set.
-# train = train.concatenate(rotated)
+train = train.concatenate(rotated)
+
+
 
 class Net():
     def __init__(self, input_shape):
         self.model = models.Sequential()
-        # Input: 200 x 200 x 1
-        # First layer is convolution with:
-        # Frame/kernel: 10 x 10, Stride: 5 x 5, Depth: 8
-        self.model.add(layers.Conv2D(8, 10, strides = 5,
-            input_shape = input_shape, activation = 'relu'))
-        # Output: 38 x 38 x 8
-        # Next layer is maxpool, Frame: 2 x 2, Strides: 2
-        self.model.add(layers.MaxPool2D(pool_size = 2))
-        # Output: 16 x 16 x 8
-        # self.model.add(layers.Dropout(0.3))
-        # Next up Conv with Frame: 3 x 3, Strides: 1, Depth: 8
-        self.model.add(layers.Conv2D(8, 1, activation = 'relu'))
-        # Output: 13 x 13 x 8
-        # Next up maxpool again. Frame: 2 x 2, Strides: 2
-        # self.model.add(layers.MaxPool2D(pool_size = 2))
-        # # Output: 19 x 19 x 8
-        # # Now, flatten
+        self.model.add(layers.Conv2D(
+            8, # filters (kept 8 bc I liked it)
+            10, # size
+            strides = 5, # step size
+            activation = 'relu',
+            input_shape = input_shape,   # To build model
+        )) # 39 x 39 x 8 📣
+        self.model.add(layers.ZeroPadding2D(
+            padding = ((1, 0), (1, 0)),
+            input_shape = input_shape,
+        )) # 40 x 40 x 8 📣
+        self.model.add(layers.MaxPool2D(
+            pool_size = 2
+        )) # 20 x 20 x 8 📣
+        self.model.add(layers.Conv2D(
+            8, # filters (kept 8 bc I liked it)
+            3, # size
+            strides = 1, # step size
+            activation = 'relu',
+        )) # 18 x 18 x 8 📣
+        self.model.add(layers.MaxPool2D(
+            pool_size = 2
+        )) # 9 x 9 x 8 = 648 📣
         self.model.add(layers.Flatten())
-        # Output length: 1352
-        self.model.add(layers.Dense(1024, activation = 'relu'))
-        self.model.add(layers.Dense(256, activation = 'relu'))
-        self.model.add(layers.Dense(64, activation = 'relu'))
-        # Softmax activation will turn values into probabilities
-        self.model.add(layers.Dense(16, activation = 'softmax'))
+        self.model.add(layers.Dense(
+            512,   # Power of 2s >
+            activation = 'relu',
+        ))
+        self.model.add(layers.Dense(
+            256,
+            activation = 'relu',
+        ))
+        self.model.add(layers.Dense(
+            64,  
+            activation = 'relu',
+        ))
+        self.model.add(layers.Dense(
+            32,  
+            activation = 'relu',
+        ))
+        self.model.add(layers.Dense(
+            16,   # Number of classes
+            activation = 'softmax',
+        ))
         # Also try CategoricalCrossentropy()
         self.loss = losses.CategoricalCrossentropy()
         self.optimizer = optimizers.SGD(learning_rate = 0.0001)
@@ -90,11 +111,12 @@ class Net():
             optimizer = self.optimizer,
             metrics = ['accuracy'],
         )
+
     def __str__(self):
         self.model.summary()
         return ""
 
-net = Net((200, 200, 1))
+net = Net((200, 200, 1))    # 1 because grayscale
 print(net)
 
 # callbacks = [
@@ -107,7 +129,7 @@ print(net)
 net.model.fit(
     train,
     batch_size = 32,
-    epochs = 40,
+    epochs = 100,
     verbose = 2,
     validation_data = test,
     validation_batch_size = 32,
